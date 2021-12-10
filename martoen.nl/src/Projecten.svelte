a39ae2d9a0d91819d8635f22bb66d5b25285baba
Martijn <martijnschermers2@gmail.com>
2021-12-10 17:02:31 +0100

    Added Projects section

---
+++ martoen.nl/src/Projecten.svelte
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<div id="projects" class="projects container">
+    <h1>Projecten</h1>
+    <div class="project-card">
+        <h2>Project 1</h2>
+        <p>Beschrijving van project 1!</p>
+
+    </div>
+</div>
+
+<style>
+    .projects {
+        flex-direction: column;
+    }
+
+    h1 { 
+        font-size: 2.5rem; 
+    }
+
+    h2 {
+        font-size: 1.75rem; 
+    }
+
+    p {
+        font-size: 1.2rem;
+    }
+
+    .project-card { 
+        padding: 2.5em;
+        background-color: var(--primary);
+        border-radius: 2em;
+        margin-top: 2.5em;
+        transform-style: preserve-3d;
+    }
+</style>
