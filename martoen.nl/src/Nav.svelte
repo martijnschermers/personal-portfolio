36443d90ee80be299321b07b3fab955c2de89016
Martijn <martijnschermers2@gmail.com>
2021-12-10 17:03:17 +0100

    Possibilty to add background-img to nav-cards

--- martoen.nl/src/Nav.svelte
+++ martoen.nl/src/Nav.svelte
@@ -1,6 +1,6 @@
 <script>
-    import { fade } from 'svelte/transition';
     import { onMount } from 'svelte';
+    import VanillaTilt from 'vanilla-tilt';
 
     let visible = false;
     onMount(() => visible = true); 
@@ -26,6 +26,8 @@
             }
         };
     }
+
+    
 </script>
 
 <div class="header">
@@ -39,17 +41,20 @@
 
     <div class="nav-list">
         {#if visible}
-            <div class="nav-card">
-                <h2>Updates</h2>
-                <p> Op dit gedeelte van de website zijn mijn meest recente Github pushes te zien. 
-					Deze updates zijn realtime, omdat er een verbinding is met de Github API. 
-					De API haalt alle push events op die binnenkomen op mijn openbare repositories!
-				</p>
-                <i class="fa fa-wrench" aria-hidden="true"></i>
-                <a href="#updates">More info...</a>
+            <div class="nav-card" data-tilt>
+                <img src="images/coding.jpeg" alt="coding">
+                <div class="content">
+                    <h2>Updates</h2>
+                    <p> Op dit gedeelte van de website zijn mijn meest recente Github pushes te zien. 
+                        Deze updates zijn realtime, omdat er een verbinding is met de Github API. 
+                        De API haalt alle push events op die binnenkomen op mijn openbare repositories!
+                    </p>
+                    <i class="fa fa-wrench" aria-hidden="true"></i>
+                    <a href="#updates">More info...</a>
+                </div>
             </div>
 
-            <div class="nav-card">
+            <div class="nav-card" data-tilt>
                 <h2>Projecten</h2>
                 <p> Mijn meest recente projecten zijn te zien op het project gedeelte van de website. 
                     Een aantal van die projecten zijn ook terug te vinden op mijn Github pagina.
@@ -59,7 +64,7 @@
                 <a href="#projects">More info...</a>
             </div>
 
-            <div class="nav-card">
+            <div class="nav-card" data-tilt>
                 <h2>About</h2>
                 <p> Als je meer te weten wilt komen over de maker van deze website, kan je de about sectie eens bekijken. 
                     Via de link hieronder kom je er direct!
@@ -68,7 +73,7 @@
                 <a href="#about">More info...</a>
             </div>
 
-            <div class="nav-card">
+            <div class="nav-card" data-tilt>
                 <h2>Contact</h2>
                 <p> Op dit gedeelte van de website zijn de platformen te zien waar ik actief op ben. 
                     Als je een vraag of opmerking hebt, kan je me het beste een berichtje sturen via Discord.
@@ -76,8 +81,6 @@
                 <i class="fa fa-phone" aria-hidden="true"></i>
                 <a href="#contact">More info...</a>
             </div>
-
-            <div class="blank"></div>
         {/if}
     </div>
 </div>
@@ -125,10 +128,24 @@
     }
 
     .nav-card {
-        margin-top: 2em;
+        margin-block: 1.5em;
         border-radius: .8em; 
         padding: .8em;
         width: 80%; 
         box-shadow: -.5em .5em var(--white);
+        overflow: hidden;
+        position: relative;
+    }
+
+    .nav-card img {
+        opacity: .6;
+        position: absolute;
+        left: 0; 
+        top: 0; 
+        width: 100%;
+    }
+
+    .content {
+        position: relative;
     }
 </style>
