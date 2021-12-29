a445c7029705df3f21838967bdf786a51a59ae0c
Martijn <martijnschermers2@gmail.com>
2021-12-29 16:49:14 +0100

    Project section progress

--- martoen.nl/src/Projecten.svelte
+++ martoen.nl/src/Projecten.svelte
@@ -24,20 +24,22 @@
 
           (async function () {
             let readmeUrl = `https://raw.githubusercontent.com/${owner}/${name}/master/README.md`;
-
             let response = await fetch(readmeUrl, {});
-
             let readme = await response.text();
+
             let html = converter.makeHtml(readme);
             console.log(html);
             project.innerHTML = html;
 
-						link.href = repo.html_url;
-						project.appendChild(link);
+            link.href = repo.html_url;
+            link.textContent = "Github";
+            project.append(link);
           })();
         }
         count++;
       });
+    } else {
+      // Show error message
     }
   })();
 </script>
@@ -50,13 +52,7 @@
     </div>
 
     <div class="content">
-      <p>
-        With this project it is possible to fetch data from a API, in this case
-        the Github API. It is possible to add authorization tokens, so that you
-        can access private repositories for example. Inside the app.js there is
-        some commented code that explains possible use cases! If you have
-        questions or found bugs, please let me know.
-      </p>
+      <p />
       <a href="#projects"><i class="fa fa-link" /> Github</a>
     </div>
   </div>
@@ -67,7 +63,7 @@
     </div>
 
     <div class="content">
-      <p></p>
+      <p />
       <a href="#projects"><i class="fa fa-link" /> Github</a>
     </div>
   </div>
@@ -78,14 +74,7 @@
     </div>
 
     <div class="content">
-      <p>
-        This system contains a login, signup and an index page. It has a
-        database connection that is setup for localhost, but you can change that
-        easily. It also comes with a SQL file which you can import into your own
-        database easily. The system is protected against SQL injections, because
-        I used prepared statements! You are free to use this and change it if
-        you want to. If you find any problems, please let me know!
-      </p>
+      <p />
       <a href="#projects"><i class="fa fa-link" /> Github</a>
     </div>
   </div>
@@ -110,26 +99,24 @@
   }
 
   a {
-    font-size: 1.2rem;
+    font-size: 2rem;
     font-weight: bold;
   }
 
   div[class^="project-card"] {
-    padding: 1.5rem;
-    background-color: var(--primary);
-    border-radius: 2em;
-    margin-top: 5.5em;
-  }
-
-  div[class^="project-card"]:nth-child(odd) {
-    background-color: var(--primary);
-    border-radius: 2em;
-    margin-top: 5.5em;
+		padding: 1.5em;
+    margin: 1.5em;
+    border-radius: 1.2em;
+    box-shadow: 0.5em 0.5em var(--white);
+    background: linear-gradient(45deg, var(--primary), var(--secondary));
+    width: 80%;
+    max-width: 756px;
+    transition: 0.3s linear;
   }
 
   .header {
     padding: 1em;
-    width: 100%;
+    width: 80%;
     background-color: var(--secondary);
     border-radius: 2em 2em 0 0;
   }
