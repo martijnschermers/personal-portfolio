49765c0905de16e1eddd4301d538a0aa33f04ee2
Martijn <martijnschermers2@gmail.com>
2022-01-02 13:03:06 +0100

    Update Updates.svelte

--- martoen.nl/src/Updates.svelte
+++ martoen.nl/src/Updates.svelte
@@ -1,15 +1,15 @@
 <script>
-  const url = "https://api.github.com/users/ms-arrow/events/public";
+  let url = "https://api.github.com/users/martijnschermers/events/public";
   let count = 0;
 
   (async function () {
-    const response = await fetch(url, {
+    let response = await fetch(url, {
       method: "GET",
       headers: new Headers({
         "Content-Type": "application/json",
       }),
     });
-    const data = await response.json();
+    let data = await response.json();
     // console.log(data);
 
     if (response.status >= 200 && response.status < 400) {
@@ -20,61 +20,96 @@
               // const img = document.querySelector(`.update-${count} img`);
               // img.src = i.actor.avatar_url;
 
-              const a = document.querySelector(`.update-${count} a`);
-              const time = document.querySelector(`.update-${count} time`);
+              let container = document.getElementById("update-cards"); 
+
+              let card = document.createElement("div"); 
+              card.setAttribute("class", "update");
+
+              let header = document.createElement("div");
+              header.setAttribute("class", "header");
+
+              let content = document.createElement("div");
+              content.setAttribute("class", "content"); 
+
+              let title = document.createElement("h2"); 
+              title.textContent = i.repo.name.substring(17, 10000); 
+
+              let description = document.createElement("p");
+              description.textContent = commit.message;
+
+              let link = document.createElement("a"); 
+              link.textContent = "Github"; 
+
+              let time = document.createElement("time"); 
 
               (async function () {
-                const response = await fetch(commit.url, {
+                let response = await fetch(commit.url, {
                   method: "GET",
                   headers: new Headers({
                     "Content-Type": "application/json",
                   }),
                 });
-                const commitData = await response.json();
+                let commits = await response.json();
 
-                a.href = commitData.html_url;
+                link.href = commits.html_url;
 
-                let date = new Date(commitData.commit.committer.date);
+                let date = new Date(commits.commit.committer.date);
                 time.textContent = date.toString().substring(4, 21);
-                time.datetime = commitData.commit.committer.date;
+                time.datetime = commits.commit.committer.date;
               })();
 
-              const p = document.querySelector(`.update-${count} p`);
-              p.textContent = commit.message;
+              header.appendChild(title);
+              content.appendChild(description);
 
-              const h2 = document.querySelector(`.update-${count} h2`);
-              h2.textContent = i.repo.name;
+              content.appendChild(link);
+              content.appendChild(time);
+
+              card.appendChild(header);
+              card.appendChild(content);
+
+              container.appendChild(card);
             });
             count++;
           }
         }
       });
     } else {
-      const errorMessage = document.querySelector(".updates h1");
+      const errorMessage = document.querySelector(".updates p");
       errorMessage.textContent = data.message;
     }
   })();
 </script>
 
 <div id="updates" class="updates container">
-  <h1>Updates</h1>
+  <div class="updates-top">
+    <h1>Updates</h1>
+    <p> Op dit gedeelte van de website zijn mijn meest recente Github pushes
+      te zien. Deze updates zijn realtime, omdat er een verbinding is met
+      de Github API. De API haalt alle push events op die binnenkomen op
+      mijn openbare repositories!
+    </p>
+  </div>
 
-  <div class="update-0">
+  <div id="update-cards">
+
+  </div>
+
+  <!-- <div class="update-0">
     <div class="header">
-      <!-- <img src="" alt="Avatar"> -->
+      <img src="" alt="Avatar">
       <h2>update_1</h2>
     </div>
 
     <div class="content">
       <p>description</p>
-      <a href="#updates"><i class="fa fa-link" aria-hidden="true" /> Github</a>
+      <a href="#updates"><i class="fa fa-link" aria-hidden="true"/> Github</a>
       <time />
     </div>
   </div>
 
   <div class="update-1">
     <div class="header">
-      <!-- <img src="" alt="Avatar"> -->
+      <img src="" alt="Avatar">
       <h2>update_2</h2>
     </div>
 
@@ -87,7 +122,7 @@
 
   <div class="update-2">
     <div class="header">
-      <!-- <img src="" alt="Avatar"> -->
+      <img src="" alt="Avatar">
       <h2>update_3</h2>
     </div>
 
@@ -100,7 +135,7 @@
 
   <div class="update-3">
     <div class="header">
-      <!-- <img src="" alt="Avatar"> -->
+      <img src="" alt="Avatar">
       <h2>update_4</h2>
     </div>
 
@@ -109,7 +144,7 @@
       <a href="#updates"><i class="fa fa-link" aria-hidden="true" /> Github</a>
       <time />
     </div>
-  </div>
+  </div> -->
 </div>
 
 <style>
@@ -118,7 +153,22 @@
     flex-direction: column;
   }
 
-  div[class^="update-"] {
+  .updates-top {
+    width: 80%;
+  }
+
+  /* div[class^="update-"] {
+    padding: 1.5em;
+    margin: 1.5em;
+    border-radius: 1.2em;
+    box-shadow: 0.5em 0.5em var(--white);
+    background: linear-gradient(45deg, var(--primary), var(--secondary));
+    width: 80%;
+    max-width: 756px;
+    transition: 0.3s linear;
+  } */
+
+  .update {
     padding: 1.5em;
     margin: 1.5em;
     border-radius: 1.2em;
