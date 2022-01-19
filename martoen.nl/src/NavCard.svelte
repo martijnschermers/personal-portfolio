d0e00a0e5fce79789691ff7b4e33e771fcdca331
Martijn <martijnschermers2@gmail.com>
2022-01-19 20:17:30 +0100

    Removed main due to component reasons

--- martoen.nl/src/NavCard.svelte
+++ martoen.nl/src/NavCard.svelte
@@ -1,54 +1,52 @@
 <script>
 </script>
 
-<main>
-  <div class="nav-card" data-tilt>
-    <img src="images/coding.jpeg" alt="coding" />
-    <div class="content">
-      <h2>Updates</h2>
-      <p>
-        Op dit gedeelte van de website zijn mijn meest recente Github pushes te
-        zien. Deze updates zijn realtime, omdat er een verbinding is met de
-        Github API. De API haalt alle push events op die binnenkomen op mijn
-        openbare repositories!
-      </p>
-      <i class="fa fa-wrench" aria-hidden="true" />
-      <a href="#updates">More info...</a>
-    </div>
-  </div>
-
-  <div class="nav-card" data-tilt>
-    <h2>Projecten</h2>
+<div class="nav-card" data-tilt>
+  <img src="images/coding.jpeg" alt="coding" />
+  <div class="content">
+    <h2>Updates</h2>
     <p>
-      Mijn meest recente projecten zijn te zien op het project gedeelte van de
-      website. Een aantal van die projecten zijn ook terug te vinden op mijn
-      Github pagina. Klik hieronder om de projecten te bekijken!
+      Op dit gedeelte van de website zijn mijn meest recente Github pushes te
+      zien. Deze updates zijn realtime, omdat er een verbinding is met de Github
+      API. De API haalt alle push events op die binnenkomen op mijn openbare
+      repositories!
     </p>
-    <i class="fa fa-cog" aria-hidden="true" />
-    <a href="#projects">More info...</a>
+    <i class="fa fa-wrench" aria-hidden="true" />
+    <a href="#updates">More info...</a>
   </div>
+</div>
 
-  <div class="nav-card" data-tilt>
-    <h2>About</h2>
-    <p>
-      Als je meer te weten wilt komen over de maker van deze website, kan je de
-      about sectie eens bekijken. Via de link hieronder kom je er direct!
-    </p>
-    <i class="fa fa-info" aria-hidden="true" />
-    <a href="#about">More info...</a>
-  </div>
+<div class="nav-card" data-tilt>
+  <h2>Projecten</h2>
+  <p>
+    Mijn meest recente projecten zijn te zien op het project gedeelte van de
+    website. Een aantal van die projecten zijn ook terug te vinden op mijn
+    Github pagina. Klik hieronder om de projecten te bekijken!
+  </p>
+  <i class="fa fa-cog" aria-hidden="true" />
+  <a href="#projects">More info...</a>
+</div>
 
-  <div class="nav-card" data-tilt>
-    <h2>Contact</h2>
-    <p>
-      Op dit gedeelte van de website zijn de platformen te zien waar ik actief
-      op ben. Als je een vraag of opmerking hebt, kan je me het beste een
-      berichtje sturen via Discord.
-    </p>
-    <i class="fa fa-phone" aria-hidden="true" />
-    <a href="#contact">More info...</a>
-  </div>
-</main>
+<div class="nav-card" data-tilt>
+  <h2>About</h2>
+  <p>
+    Als je meer te weten wilt komen over de maker van deze website, kan je de
+    about sectie eens bekijken. Via de link hieronder kom je er direct!
+  </p>
+  <i class="fa fa-info" aria-hidden="true" />
+  <a href="#about">More info...</a>
+</div>
+
+<div class="nav-card" data-tilt>
+  <h2>Contact</h2>
+  <p>
+    Op dit gedeelte van de website zijn de platformen te zien waar ik actief op
+    ben. Als je een vraag of opmerking hebt, kan je me het beste een berichtje
+    sturen via Discord.
+  </p>
+  <i class="fa fa-phone" aria-hidden="true" />
+  <a href="#contact">More info...</a>
+</div>
 
 <style>
   h2 {
