e72e7cff8ee5656e1b41b231428e7f1f8add84e7
Martijn <martijnschermers2@gmail.com>
2022-04-03 22:13:52 +0200

    Create mockup for About section

--- martoen.nl/src/About.svelte
+++ martoen.nl/src/About.svelte
@@ -1,5 +1,20 @@
-<script></script>
+<script>
+</script>
 
-<main></main>
+<main>
+  <div id="about" class="container">
+    <h1>About Me</h1>
+
+    <div class="card">
+      <p> 
+        Hallo daar! Leuk dat je op mijn site terecht bent gekomen. Ik hoop dat je 't wat vindt. 
+        Mijn naam is Martijn en in het dagelijks leven ben ik regelmatig bezig met verschillende technieken.
+        Ik zit momenteel in het eerste jaar van Informatica op HBO niveau.
+        Vandaar mijn interesse in programmeren in mijn vrije tijd. Als ik een leuk ideetje heb, probeer ik dat uit te werken.
+        Een aantal voorbeelden hiervan zijn ook te zien op deze website.
+      </p>
+    </div>
+  </div>
+</main>
 
 <style></style>
