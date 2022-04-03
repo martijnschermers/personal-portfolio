bc0e2d1ea7857e79fbcfc6d0bfd6f1d93f89182f
Martijn <martijnschermers2@gmail.com>
2022-04-03 21:59:44 +0200

    Removed styles & added project

--- martoen.nl/src/Projects.svelte
+++ martoen.nl/src/Projects.svelte
@@ -1,54 +1,38 @@
 <script>
+  import { fly } from "svelte/transition";
+  import { onMount } from "svelte";
+
+  let visible = false;
+  onMount(() => (visible = true));
 </script>
 
 <div id="projects" class="projects container">
   <h1>Projecten</h1>
-  <div class="project-card">
-    <div class="header">
-      <h2>JavaScript API</h2>
+  {#if visible}
+    <div transition:fly={{ y: 200, duration: 2000 }} class="card">
+      <h2>Schooster</h2>
+
+      <p>
+        Dit project is een website gemaakt met het Django framework. Deze
+        website laat de roosters zien van mij en mijn zusjes.
+      </p>
+      <a href="https://schooster.nl/"><i class="fa fa-link" /> Website</a>
     </div>
 
-    <div class="content">
+    <div transition:fly={{ y: 200, duration: 2000 }} class="card">
+      <h2>JavaScript API</h2>
+
       <p>
         Door dit kleine project ben ik meer te weten gekomen over werken met
         API's. In dit geval ging het over een REST API die JSON data teruggaf.
+        Ik heb deze API ook verwerkt in deze website.
       </p>
-      <a href="#projects"><i class="fa fa-link" /> Github</a>
+      <a href="https://github.com/martijnschermers/javascript-api"
+        ><i class="fa fa-link" /> Github</a
+      >
     </div>
-  </div>
+  {/if}
 </div>
 
 <style>
-  .projects {
-    margin-top: 5em;
-    flex-direction: column;
-  }
-
-  h2 {
-    font-size: 1.75rem;
-  }
-
-  p {
-    font-size: 1.2rem;
-    margin-bottom: 0.5rem;
-  }
-
-  a {
-    font-size: 1.5rem;
-    font-weight: bold;
-  }
-
-  .project-card {
-    padding: 1.5em;
-    border-radius: 1.2em;
-    box-shadow: 0.5em 0.5em var(--white);
-    background: linear-gradient(45deg, var(--primary), var(--secondary));
-    width: 80%;
-    transition: 0.3s linear;
-  }
-
-  .header {
-    padding: 1em;
-    border-radius: 2em 2em 0 0;
-  }
 </style>
