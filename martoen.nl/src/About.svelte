e6a75c6d8ea76fa5f7e33e4b5cb179e1e62814c6
Martijn <martijnschermers2@gmail.com>
2022-04-04 15:11:14 +0200

    Removed <main>

--- martoen.nl/src/About.svelte
+++ martoen.nl/src/About.svelte
@@ -1,20 +1,18 @@
 <script>
 </script>
 
-<main>
-  <div id="about" class="container">
-    <h1>About Me</h1>
+<div id="about" class="container">
+  <h1>About Me</h1>
 
-    <div class="card">
-      <p> 
-        Hallo daar! Leuk dat je op mijn site terecht bent gekomen. Ik hoop dat je 't wat vindt. 
-        Mijn naam is Martijn en in het dagelijks leven ben ik regelmatig bezig met verschillende technieken.
-        Ik zit momenteel in het eerste jaar van Informatica op HBO niveau.
-        Vandaar mijn interesse in programmeren in mijn vrije tijd. Als ik een leuk ideetje heb, probeer ik dat uit te werken.
-        Een aantal voorbeelden hiervan zijn ook te zien op deze website.
-      </p>
-    </div>
+  <div class="card">
+    <p> 
+      Hallo daar! Leuk dat je op mijn site terecht bent gekomen. Ik hoop dat je 't wat vindt. 
+      Mijn naam is Martijn en in het dagelijks leven ben ik regelmatig bezig met verschillende technieken.
+      Ik zit momenteel in het eerste jaar van Informatica op HBO niveau.
+      Vandaar mijn interesse in programmeren in mijn vrije tijd. Als ik een leuk ideetje heb, probeer ik dat uit te werken.
+      Een aantal voorbeelden hiervan zijn ook te zien op deze website.
+    </p>
   </div>
-</main>
+</div>
 
 <style></style>
--- martoen.nl/src/Footer.svelte
+++ martoen.nl/src/Footer.svelte
@@ -1,5 +1,3 @@
 <script></script>
 
-<main></main>
-
 <style></style>
