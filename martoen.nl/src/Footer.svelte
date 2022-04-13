329e17087c40fda72ad0e991bf07ebde7015175d
Martijn <martijnschermers2@gmail.com>
2022-04-13 19:36:46 +0200

    Created basic footer

--- martoen.nl/src/Footer.svelte
+++ martoen.nl/src/Footer.svelte
@@ -1,3 +1,28 @@
-<script></script>
+<script>
+  const icons = [];
 
-<style></style>
+  icons.push("fa fa-github");
+  icons.push("fa fa-linkedin");
+  icons.push("fa fa-steam");
+</script>
+
+<footer>
+  {#each icons as icon}
+    <i class={icon}/>
+  {/each}
+</footer>
+
+<style>
+  footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(-45deg, var(--primary), var(--secondary));
+  }
+
+  i {
+    font-size: 2rem;
+    color: var(--white);
+    margin: 1rem;
+  }
+</style>
