b20259f1923dd173eb4dd3ebd4431648260d5acc
Martijn <martijnschermers2@gmail.com>
2022-04-13 22:30:22 +0200

    Added small text to footer

--- martoen.nl/src/Footer.svelte
+++ martoen.nl/src/Footer.svelte
@@ -7,15 +7,22 @@
 </script>
 
 <footer>
-  {#each icons as icon}
-    <a href="{icon.link}" aria-label="{icon.label}"><i class={icon.class}/></a>
-  {/each}
+  <div>
+    {#each icons as icon}
+      <a href={icon.link} aria-label={icon.label}><i class={icon.class}/></a>
+    {/each}
+  </div>
+
+  <p>
+    Gemaakt met <i class="fa fa-heart"></i> en <a href="https://svelte.dev" aria-label="Link to Svelte website"><img src="./images/svelte.png" alt="Svelte logo"></a>
+  </p>
 </footer>
 
 <style>
   footer {
     display: flex;
     justify-content: center;
+    flex-direction: column;
     align-items: center;
   }
 
@@ -23,5 +30,23 @@
     font-size: 2rem;
     color: var(--white);
     margin: 1rem;
+  }
+
+  p {
+    font-size: 1.4rem; 
+    text-align: center;
+  }
+
+  img {
+    vertical-align: middle;
+    width: 22px;
+    height: auto;
+    margin: .5rem; 
+  }
+
+  .fa-heart {
+    font-size: 1.4rem;
+    color: red;
+    margin: .5rem; 
   }
 </style>
