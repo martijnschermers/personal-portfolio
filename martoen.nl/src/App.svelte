36ee2ae22d645a0ebc3f3f3ea397389a9e2f9e3d
Martijn <martijnschermers2@gmail.com>
2022-05-06 10:43:49 +0200

    Formatted file

--- martoen.nl/src/App.svelte
+++ martoen.nl/src/App.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
-	import Nav from './Nav.svelte';
-	import About from './About.svelte';
-	import Footer from './Footer.svelte';
-	import Projects from './Projects.svelte';
-	import Updates from './Updates.svelte';
+  import Nav from "./Nav.svelte";
+  import About from "./About.svelte";
+  import Footer from "./Footer.svelte";
+  import Projects from "./Projects.svelte";
+  import Updates from "./Updates.svelte";
 </script>
 
 <main>
-	<Nav/>
-	<Updates/>
-	<Projects/>
-	<About/>
-	<Footer/>
+  <Nav />
+  <Updates />
+  <Projects />
+  <About />
+  <Footer />
 </main>
 
 <style>
-
 </style>
--- martoen.nl/src/Footer.svelte
+++ martoen.nl/src/Footer.svelte
@@ -1,20 +1,35 @@
 <script>
   let icons = [];
 
-  icons.push({ class: "fa fa-github", link: "https://github.com/martijnschermers", label: "Github profile link" });
-  icons.push({ class: "fa fa-linkedin", link: "https://www.linkedin.com/in/martijn-schermers/", label: "LinkedIn profile link" });
-  icons.push({ class: "fa fa-steam", link: "https://steamcommunity.com/profiles/76561198329654915/", label: "Steam profile link" });
+  icons.push({
+    class: "fa fa-github",
+    link: "https://github.com/martijnschermers",
+    label: "Github profile link",
+  });
+  icons.push({
+    class: "fa fa-linkedin",
+    link: "https://www.linkedin.com/in/martijn-schermers/",
+    label: "LinkedIn profile link",
+  });
+  icons.push({
+    class: "fa fa-steam",
+    link: "https://steamcommunity.com/profiles/76561198329654915/",
+    label: "Steam profile link",
+  });
 </script>
 
 <footer>
   <div>
     {#each icons as icon}
-      <a href={icon.link} aria-label={icon.label}><i class={icon.class}/></a>
+      <a href={icon.link} aria-label={icon.label}><i class={icon.class} /></a>
     {/each}
   </div>
 
   <p>
-    Gemaakt met <i class="fa fa-heart"></i> en <a href="https://svelte.dev" aria-label="Link to Svelte website"><img src="./images/svelte.png" alt="Svelte logo"></a>
+    Gemaakt met <i class="fa fa-heart" /> en
+    <a href="https://svelte.dev" aria-label="Link to Svelte website">
+      <img src="./images/svelte.png" alt="Svelte logo" />
+      </a>
   </p>
 </footer>
 
@@ -32,7 +47,7 @@
   }
 
   p {
-    font-size: 1.4rem; 
+    font-size: 1.4rem;
     text-align: center;
   }
 
@@ -40,12 +55,13 @@
     vertical-align: middle;
     width: 22px;
     height: auto;
-    margin: .5rem; 
+    margin: 0.5rem;
+    border-radius: 0;
   }
 
   .fa-heart {
     font-size: 1.4rem;
     color: red;
-    margin: .5rem; 
+    margin: 0.5rem;
   }
 </style>
