1f597b51a352a2d7e8570f65fe0578384249f660
Martijn <martijnschermers2@gmail.com>
2022-05-06 10:47:25 +0200

    Optimised date&name formatting, added profile image

--- martoen.nl/src/Updates.svelte
+++ martoen.nl/src/Updates.svelte
@@ -1,10 +1,10 @@
 <script>
-  let url = "https://api.github.com/users/martijnschermers/events/public";
+  const URL = "https://api.github.com/users/martijnschermers/events/public";
   let commits = [];
   let count = 0;
 
   (async function () {
-    let response = await fetch(url, {
+    let response = await fetch(URL, {
       method: "GET",
       headers: new Headers({
         "Content-Type": "application/json",
@@ -14,11 +14,13 @@
 
     if (response.ok) {
       data.forEach(async function (i) {
-        if ((data.type = "PushEvent" && count < 4)) {
+        if ((data.type = "PushEvent" && count < 5)) {
           if (i.payload.commits !== undefined) {
             await Promise.all(
               i.payload.commits.map(async function (commit) {
                 let placeholder = {};
+                placeholder.commit = commit;
+                placeholder.repo = i.repo.name.split("/")[1];
 
                 (async function () {
                   let response = await fetch(commit.url, {
@@ -29,11 +31,10 @@
                   });
                   let commitData = await response.json();
 
-                  placeholder.commit = commit;
-
                   let date = new Date(commitData.commit.committer.date);
-                  placeholder.date = date.toString().substring(4, 21);
+                  placeholder.date = date.toLocaleDateString("nl-NL");
                   placeholder.html_url = commitData.html_url;
+                  placeholder.image = commitData.author.avatar_url;
 
                   commits = [...commits, placeholder];
                 })();
@@ -44,7 +45,7 @@
         }
       });
     } else {
-      let errorMessage = document.querySelector(".updates p");
+      let errorMessage = document.querySelector(".section-info");
       errorMessage.textContent = data.message;
     }
   })();
@@ -52,26 +53,44 @@
 
 <div id="updates" class="container">
   <h1>Updates</h1>
-  <p class="section-info">
-    Commits naar mijn openbare Github repositories: 
-  </p>
+  <p class="section-info">Commits naar mijn openbare Github repositories:</p>
 
-  {#each commits as commit}
-    <div class="card">
-      <h2>{commit.commit.author.name}</h2>
-    
-      <p>{commit.commit.message}</p>
-      <div class="content">
-        <a href={commit.html_url} class="underline"><i class="fa fa-link" /> Github</a>
-        <time>{commit.date}</time>
+  <div class="wrapper">
+    {#each commits as commit}
+      <div class="card">
+        <div class="header">
+          <img src={commit.image} alt="Avatar of author" />
+          <h2>{commit.repo}</h2>
+        </div>
+
+        <p>{commit.commit.message}</p>
+        <div class="content">
+          <a href={commit.html_url} class="underline">
+            <i class="fa fa-link" /> Github
+          </a>
+          <time>{commit.date}</time>
+        </div>
       </div>
-    </div>
-  {:else}
-    <p>Er zijn geen recente commits beschikbaar.</p>
-  {/each}
+    {/each}
+  </div>
 </div>
 
 <style>
+  time {
+    font-size: 1.1rem;
+  }
+
+  h2 {
+    font-size: 1.8rem;
+  }
+
+  img {
+    width: 64px;
+    height: 64px;
+    border-radius: 50%;
+    margin-right: 1rem;
+  }
+
   .content {
     display: flex;
     justify-content: space-between;
@@ -81,7 +100,38 @@
     text-align: center;
   }
 
-  time {
-    font-size: 1.1rem;
+  .header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+
+  .wrapper {
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 1rem;
+  }
+
+  .card {
+    max-width: 30em;
+  }
+
+  @media (max-width: 768px) {
+    .wrapper {
+      grid-template-columns: auto;
+    }
+
+    img {
+      width: 48px;
+      height: 48px;
+    }
+
+    h2 {
+      font-size: 1.5rem;
+    }
+
+    .card {
+      width: 10em;
+    }
   }
 </style>
