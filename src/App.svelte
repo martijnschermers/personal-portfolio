63cfb95e232fad7821c6f7a234c684a22c49c818
Martijn <martijnschermers2@gmail.com>
2022-06-14 22:04:09 +0200

    Added dividers

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,21 @@
 <script>
-  import Nav from "./Nav.svelte";
-  import About from "./About.svelte";
-  import Footer from "./Footer.svelte";
-  import Projects from "./Projects.svelte";
-  import Updates from "./Updates.svelte";
+	import Nav from "./Nav.svelte";
+	import Hero from "./Hero.svelte";
+	import About from "./About.svelte";
+	import Footer from "./Footer.svelte";
+	import Projects from "./Projects.svelte";
+	import Updates from "./Updates.svelte";
 </script>
 
 <main>
-  <Nav />
-  <Updates />
-  <Projects />
-  <About />
-  <Footer />
+	<Nav />
+	<Hero />
+	<Updates />
+	<div class="divider" />
+	<Projects />
+	<div class="divider" />
+	<About />
+	<Footer />
 </main>
 
 <style>
