df4cf47b9a2e397276be5bc2f40a697c31e0a4c3
martijnschermers <martijnschermers2@gmail.com>
2022-07-11 16:49:26 +0200

    Remove: ; behind target

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import App from './App.svelte';
 import './assets/global.css';
 
 const app = new App({
-  target: document.getElementById('app');
+  target: document.getElementById('app')
 })
 
 export default app;
