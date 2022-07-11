70b8de199a916078f657e19fda23aff1c429ea6e
martijnschermers <martijnschermers2@gmail.com>
2022-07-11 17:16:32 +0200

    Add: ProjectCard

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,42 +1,90 @@
 <script>
-  import { fly } from "svelte/transition";
   import { onMount } from "svelte";
+  import ProjectCard from "./components/ProjectCard.svelte";
 
   let visible = false;
   onMount(() => (visible = true));
+
+  let projects = [];
+  projects.push({
+    title: "Schooster",
+    description: `Dit project is een website gemaakt met het Django framework. Deze website laat de roosters zien van mij en mijn naasten.`,
+    type: "Website",
+    link: "https://schooster.nl",
+    badges: [
+      {
+        name: "Django",
+        link: "https://www.djangoproject.com/",
+        color: "var(--primary)",
+      },
+      {
+        name: "Sass",
+        link: "https://sass-lang.com/",
+        color: "var(--primary)",
+      },
+      {
+        name: "Bootstrap",
+        link: "https://getbootstrap.com/",
+        color: "var(--primary)",
+      },
+    ],
+  });
+
+  projects.push({
+    title: "Schooster",
+    description: `Dit project is een website gemaakt met het Django framework. Deze website laat de roosters zien van mij en mijn naasten.`,
+    type: "Website",
+    link: "https://schooster.nl",
+    badges: [
+      {
+        name: "Django",
+        link: "https://www.djangoproject.com/",
+        color: "var(--primary)",
+      },
+      {
+        name: "Sass",
+        link: "https://sass-lang.com/",
+        color: "var(--primary)",
+      },
+      {
+        name: "Bootstrap",
+        link: "https://getbootstrap.com/",
+        color: "var(--primary)",
+      },
+    ],
+  });
+
+  projects.push({
+    title: "Schooster",
+    description: `Dit project is een website gemaakt met het Django framework. Deze website laat de roosters zien van mij en mijn naasten.`,
+    type: "Website",
+    link: "https://schooster.nl",
+    badges: [
+      {
+        name: "Django",
+        link: "https://www.djangoproject.com/",
+        color: "var(--primary)",
+      },
+      {
+        name: "Sass",
+        link: "https://sass-lang.com/",
+        color: "var(--primary)",
+      },
+      {
+        name: "Bootstrap",
+        link: "https://getbootstrap.com/",
+        color: "var(--primary)",
+      },
+    ],
+  });
 </script>
 
 <div id="projects" class="container">
   <h1>Projecten</h1>
-  {#if visible}
-    <div transition:fly={{ y: 200, duration: 2000 }}>
-      <h2>Schooster</h2>
 
-      <p>
-        Dit project is een website gemaakt met het Django framework. Deze
-        website laat de roosters zien van mij en mijn naasten.
-      </p>
-      <a href="https://schooster.nl/" class="underline">
-        <i class="fa fa-link" /> Website
-      </a>
-    </div>
-
-    <div transition:fly={{ y: 200, duration: 2000 }}>
-      <h2>JavaScript API</h2>
-
-      <p>
-        Door dit kleine project ben ik meer te weten gekomen over werken met
-        API's. In dit geval ging het over een REST API die JSON data teruggaf.
-        Ik heb deze API ook verwerkt in deze website.
-      </p>
-      <a
-        href="https://github.com/martijnschermers/javascript-api"
-        class="underline"
-      >
-        <i class="fa fa-link" />Github
-      </a>
-    </div>
-  {/if}
+  {#each projects as project}
+    <ProjectCard {project} />
+  {/each}
 </div>
 
 <style>
