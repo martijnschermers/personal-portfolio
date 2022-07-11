56612425ae1c5aa596f6f957cebaffe2115ce2b3
martijnschermers <martijnschermers2@gmail.com>
2022-07-11 16:48:52 +0200

    Add: working theme switcher

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,27 +1,40 @@
 <script>
   import { onMount } from "svelte";
-	import { fade, draw, fly } from 'svelte/transition';
-	// import { shape } from './shape.js';
+  import { draw, fly } from "svelte/transition";
+  import { moon, sun } from "./svg.js";
 
   let visible = false;
   onMount(() => (visible = true));
 
-  let icon = "fa fa-moon-o";
-  function toggle() {
-    window.document.body.classList.toggle('dark-mode');
-    if (window.document.body.classList.contains('dark-mode')) {
-      icon = "fa fa-sun-o";
-    } else {
-      icon = "fa fa-moon-o";
-    }
+  let theme = localStorage.getItem("theme");
+
+  if (theme) {
+    document.documentElement.setAttribute("color-mode", theme);
+  } else {
+    theme = document.documentElement.getAttribute("color-mode");
   }
-  
+
+  let icon = theme === "dark" ? sun : moon;
+  function toggle() {
+    if (theme === "dark") {
+      theme = "light";
+      icon = moon;
+      document.documentElement.setAttribute("color-mode", theme);
+      localStorage.setItem("theme", theme);
+      return;
+    }
+
+    theme = "dark";
+    icon = sun;
+    document.documentElement.setAttribute("color-mode", theme);
+    localStorage.setItem("theme", theme);
+  }
 </script>
 
 <div id="header" class="header container">
-  <div class="nav-bar">
+  <nav>
     {#if visible}
-    <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 124">
+      <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 124">
       <g out:fade="{{duration: 200}}" opacity=0.2>
         <image
           in:draw="{{duration: 1000}}"
@@ -31,51 +44,76 @@
       </g>
     </svg> -->
 
-      <img src="./images/logo.svg" alt="Logo">
+      <img src="./images/logo.svg" alt="Logo" />
     {/if}
 
     <div class="middle-nav">
       <ul>
-        <li><a href="#updates" class="underline"><i class="fa fa-wrench" aria-hidden="true"/> Updates</a></li>
-        <li><a href="#projects" class="underline"><i class="fa fa-cog" aria-hidden="true" /> Projecten</a></li>
-        <li><a href="#about" class="underline"><i class="fa fa-info" aria-hidden="true"/> About</a></li>
+        <li>
+          <a href="#updates" class="underline"
+            ><i class="fa fa-wrench" aria-hidden="true" /> Updates</a
+          >
+        </li>
+        <li>
+          <a href="#projects" class="underline"
+            ><i class="fa fa-cog" aria-hidden="true" /> Projecten</a
+          >
+        </li>
+        <li>
+          <a href="#about" class="underline"
+            ><i class="fa fa-info" aria-hidden="true" /> About</a
+          >
+        </li>
       </ul>
     </div>
 
     <div class="side-nav">
       <ul>
-        <li><a href="https://github.com/martijnschermers" aria-label="Github profile link"><i class="fa fa-github"></i></a></li>
-        <li><button on:click={toggle} aria-label="Darkmode toggle"><i class="{icon}"></i></button></li>
+        <li>
+          <a
+            href="https://github.com/martijnschermers"
+            aria-label="Github profile link"
+          >
+            <i class="fa fa-github" />
+          </a>
+        </li>
+        <li>
+          {#if visible}
+            <button on:click={toggle} aria-label="Darkmode toggle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path in:draw={{ duration: 1000 }} d={icon} />
+              </svg>
+            </button>
+          {/if}
+        </li>
       </ul>
     </div>
-  </div>
+  </nav>
 
   {#if visible}
     <div transition:fly={{ y: 500, duration: 2000 }} class="welcome">
       <h2>Welkom op mijn website!</h2>
-      <p>
-        Hier vind je mijn recente werk, projecten en informatie over mij. 
-      </p>
+      <p>Hier vind je mijn recente werk, projecten en informatie over mij.</p>
     </div>
   {/if}
 </div>
 
 <style>
-  ul {
-		margin-block: 1em; 
-		display: flex;
-	}
-
-  li {
-		margin-inline: .8em;
-		font-size: 1.6rem; 
-    opacity: .75;
-    transition: linear 0.3s;
-    text-transform: uppercase;
-	}
-
-  li:hover {
-    opacity: 1;
+  nav {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1em;
   }
 
   img {
@@ -84,10 +122,10 @@
     padding: 1em;
   }
 
-  /* path {
-		fill: white;
-		opacity: 1;
-	} */
+  svg {
+    width: 32px;
+    aspect-ratio: 1;
+  }
 
   a {
     font-size: 1.6rem;
@@ -108,22 +146,10 @@
 
   .header {
     display: flex;
-    background-image: url('/images/bg.svg');
+    background-image: url("/images/bg.svg");
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
-  }
-
-  .nav-bar {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 1;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1em;
   }
 
   .welcome {
