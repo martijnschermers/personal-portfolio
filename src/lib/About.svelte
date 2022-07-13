a9927a86963bac574931e98f8c6ac2f04669af58
martijnschermers <martijnschermers2@gmail.com>
2022-07-13 14:19:50 +0200

    Add: basic styling to About section

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -7,11 +7,19 @@
   <p>
     Hallo daar! Leuk dat je op mijn site terecht bent gekomen. Ik hoop dat je 't
     wat vindt. Mijn naam is Martijn en in het dagelijks leven ben ik regelmatig
-    bezig met verschillende technieken. Ik zit momenteel in het eerste jaar van
-    Informatica op HBO niveau. Vandaar mijn interesse in programmeren in mijn
+    bezig met verschillende technieken. Ik zit momenteel op de opleiding
+    Informatica op HBO niveau, vandaar mijn interesse in programmeren in mijn
     vrije tijd. Als ik een leuk ideetje heb, probeer ik dat uit te werken. Een
     aantal voorbeelden hiervan zijn ook te zien op deze website.
   </p>
 </div>
 
-<style></style>
+<style>
+  p {
+    padding: 1rem;
+    border-radius: 0.5rem;
+    background-color: var(--base-300);
+    box-shadow: 0.25rem 0.25rem 0.25rem var(--primary);
+    margin: 1rem;
+  }
+</style>
