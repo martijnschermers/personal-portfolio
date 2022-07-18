867e5447de89e0def691322550f4f4ff403ac277
martijnschermers <martijnschermers2@gmail.com>
2022-07-18 13:51:15 +0200

    Moved svgs to Nav component

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,7 +1,6 @@
 <script>
   import { onMount } from "svelte";
   import { draw, fly } from "svelte/transition";
-  import { moon, sun } from "./svg.js";
 
   let visible = false;
   onMount(() => (visible = true));
@@ -14,18 +13,18 @@
     theme = document.documentElement.getAttribute("color-mode");
   }
 
-  let icon = theme === "dark" ? sun : moon;
+  let icon = theme === "dark" ? "sun" : "moon";
   function toggle() {
     if (theme === "dark") {
       theme = "light";
-      icon = moon;
+      icon = "moon";
       document.documentElement.setAttribute("color-mode", theme);
       localStorage.setItem("theme", theme);
       return;
     }
 
     theme = "dark";
-    icon = sun;
+    icon = "sun";
     document.documentElement.setAttribute("color-mode", theme);
     localStorage.setItem("theme", theme);
   }
@@ -33,19 +32,7 @@
 
 <div id="header" class="header container">
   <nav>
-    {#if visible}
-      <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 103 124">
-      <g out:fade="{{duration: 200}}" opacity=0.2>
-        <image
-          in:draw="{{duration: 1000}}"
-          style="stroke:#ff3e00; stroke-width: 1.5"
-          xlink:href={shape}
-        />
-      </g>
-    </svg> -->
-
-      <img src="./images/logo.svg" alt="Logo" />
-    {/if}
+    <img src="./images/logo.svg" alt="Logo" />
 
     <div class="middle-nav">
       <ul>
@@ -80,14 +67,31 @@
         <li>
           {#if visible}
             <button on:click={toggle} aria-label="Darkmode toggle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path in:draw={{ duration: 1000 }} d={icon} />
-              </svg>
+              {#if icon === "moon"}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    in:draw={{ duration: 1000 }}
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  />
+                </svg>
+              {:else}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    in:draw={{ duration: 1000 }}
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                </svg>
+              {/if}
             </button>
           {/if}
         </li>
