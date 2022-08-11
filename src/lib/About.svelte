51dcf8797ea327426b71ddf3d5e308245b6f7015
martijnschermers <martijnschermers2@gmail.com>
2022-08-11 15:20:17 +0200

    Small content change, h1 became h2

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -2,13 +2,13 @@
 </script>
 
 <div id="about" class="container">
-  <h1>About Me</h1>
+  <h2>About Me</h2>
 
   <p>
     Hallo daar! Leuk dat je op mijn site terecht bent gekomen. Ik hoop dat je 't
     wat vindt. Mijn naam is Martijn en in het dagelijks leven ben ik regelmatig
-    bezig met verschillende technieken. Ik zit momenteel op de opleiding
-    Informatica op HBO niveau, vandaar mijn interesse in programmeren in mijn
+    bezig met verschillende technieken. Ik volg momenteel de opleiding
+    Informatica aan hogeschool Avans, vandaar mijn interesse in programmeren in mijn
     vrije tijd. Als ik een leuk ideetje heb, probeer ik dat uit te werken. Een
     aantal voorbeelden hiervan zijn ook te zien op deze website.
   </p>
@@ -21,5 +21,9 @@
     background-color: var(--base-300);
     box-shadow: 0.25rem 0.25rem 0.25rem var(--primary);
     margin: 1rem;
+  }
+
+  div {
+    max-width: 50rem;
   }
 </style>
