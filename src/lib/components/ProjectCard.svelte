c07a6706908cb9b16f1f317cf2d99eac60158bb8
martijnschermers <martijnschermers2@gmail.com>
2022-08-11 15:21:54 +0200

    Update: h1 to h2, fontawesome to svg

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -10,7 +10,7 @@
 
 {#if visible}
   <div class="project" transition:fly={{ y: 200, duration: 2000 }}>
-    <h2>{project.title}</h2>
+    <h3>{project.title}</h3>
 
     <p>
       {project.description}
@@ -18,20 +18,29 @@
 
     <div class="links">
       <a href={project.link} class="underline">
-        <i class="fa fa-link" />
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+        </svg>
         {project.type}
       </a>
 
-      {#each project.badges as badge}
-        <div class="badge">
-          <a href={badge.link} style="color: {badge.color}">{badge.name}</a>
-        </div>
-      {/each}
+      <div class="badges">
+        {#each project.badges as badge}
+          <div class="badge">
+            <a href={badge.link} style="color: {badge.color}">{badge.name}</a>
+          </div>
+        {/each}
+      </div>
     </div>
   </div>
 {/if}
 
 <style>
+  svg {
+    vertical-align: middle;
+    width: 1.5rem;
+  }
+
   .underline {
     display: flex;
   }
@@ -51,12 +60,20 @@
     align-items: center;
   }
 
+  .badges {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+  
   .badge {
-    padding: 0.25rem;
-    background-color: var(--base-300);
+    padding: 0.5rem;
+    background-color: var(--base);
+    border-radius: .5rem;
+    margin-inline: 0.25rem;
   }
 
   .badge a {
-    font-size: 0.75rem;
+    font-size: 1rem;
   }
 </style>
--- src/lib/components/UpdateCard.svelte
+++ src/lib/components/UpdateCard.svelte
@@ -7,13 +7,16 @@
     <a href={commit.profile} alt="Link to author profile">
       <img src={commit.image} alt="Avatar of author" />
     </a>
-    <h2>{commit.repo}</h2>
+    <h3>{commit.repo}</h3>
   </div>
 
   <p>{commit.message}</p>
   <div class="content">
     <a href={commit.html_url} class="underline">
-      <i class="fa fa-link" /> Github
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1" />
+      </svg>
+      Github
     </a>
     <time>{commit.date}</time>
   </div>
@@ -24,7 +27,7 @@
     font-size: 1.1rem;
   }
 
-  h2 {
+  h3 {
     font-size: 1.5rem;
   }
 
@@ -67,7 +70,7 @@
       height: 64px;
     }
 
-    h2 {
+    h3 {
       font-size: 1.5rem;
     }
   }
