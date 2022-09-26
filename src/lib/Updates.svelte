652a324b2d63dad29c4cfb10804b9b8a6b1210b6
Martijn <martijnschermers2@gmail.com>
2022-09-26 21:33:36 +0200

    Remove: await Promise.all

--- src/lib/Updates.svelte
+++ src/lib/Updates.svelte
@@ -23,41 +23,39 @@
     data.forEach(async function (i) {
       if ((data.type = "PushEvent")) {
         if (i.payload.commits !== undefined) {
-          await Promise.all(
-            i.payload.commits.forEach(async function (commit) {
-              let placeholder = {};
+          i.payload.commits.forEach(async function (commit) {
+            let placeholder = {};
 
-              if (commit.message.includes("\n")) {
-                placeholder.message = commit.message.split("\n")[0];
-              } else {
-                placeholder.message = commit.message;
+            if (commit.message.includes("\n")) {
+              placeholder.message = commit.message.split("\n")[0];
+            } else {
+              placeholder.message = commit.message;
+            }
+
+            placeholder.repo = i.repo.name.split("/")[1];
+
+            (async function () {
+              let response = await fetch(commit.url, {
+                method: "GET",
+                headers: new Headers({
+                  "Content-Type": "application/json",
+                }),
+              });
+              let commitData = await response.json();
+
+              let date = new Date(commitData.commit.committer.date);
+              placeholder.date = date.toLocaleDateString("nl-NL");
+              placeholder.html_url = commitData.html_url;
+              placeholder.image = commitData.author.avatar_url;
+              placeholder.profile = commitData.author.html_url;
+
+              if (commits.length < 4) {
+                commits = [...commits, placeholder];
               }
-
-              placeholder.repo = i.repo.name.split("/")[1];
-
-              (async function () {
-                let response = await fetch(commit.url, {
-                  method: "GET",
-                  headers: new Headers({
-                    "Content-Type": "application/json",
-                  }),
-                });
-                let commitData = await response.json();
-
-                let date = new Date(commitData.commit.committer.date);
-                placeholder.date = date.toLocaleDateString("nl-NL");
-                placeholder.html_url = commitData.html_url;
-                placeholder.image = commitData.author.avatar_url;
-                placeholder.profile = commitData.author.html_url;
-
-                if (commits.length < 4) {
-                  commits = [...commits, placeholder];
-                }
-              })();
-            })
-          );
-        }
-      }
+            })();
+          });
+        };
+      };
     });
   })();
 </script>
